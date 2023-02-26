548ede3c4a65601d7225eb04c4b87d4a6f8c67e3
itzhaolei <itzhaolei@foxmail.com>
2023-02-27 01:44:17 +0800

    优化

--- BoYi/NewSection/MyNew/设置/SetNewViewController.m
+++ BoYi/NewSection/MyNew/设置/SetNewViewController.m
@@ -11,6 +11,7 @@
 #import "MyDataNewViewController.h"
 #import "UserNumberNewViewController.h"
 #import "ChooseSettingViewController.h"
+#import "ZLHTTPSessionManager.h"
 
 @interface SetNewViewController ()
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *topInset;
@@ -48,6 +49,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)closeUserAction:(UIButton *)sender {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"注销喜顾问账号"
+                                   message:@"账号注销后不可恢复，请您谨慎操作。注销成功后，您将无法登录或使用原账号，账号内的信息和权益将无法找回。"
+                                   preferredStyle:UIAlertControllerStyleAlert];
+     
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel
+       handler:^(UIAlertAction * action) {}];
+     
+    [alert addAction:defaultAction];
+    
+    defaultAction = [UIAlertAction actionWithTitle:@"已清楚，确定注销" style:UIAlertActionStyleDefault
+       handler:^(UIAlertAction * action) {
+        [self closeUserRequest];
+    }];
+     
+    [alert addAction:defaultAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
+/// 注销账号
+- (void)closeUserRequest {
+    NSMutableDictionary *dictM = [NSMutableDictionary new];
+    dictM[@"token"] = [UserDataNew sharedManager].userInfoModel.token.token;
+    dictM[@"userid"] = @([UserDataNew sharedManager].userInfoModel.token.userid);
+    dictM[@"status"] = @(2);
+    [ZLHTTPSessionManager requestDataWithUrlPath:@"http://www.xiguwen520.com/appapi/cancel/index" Params:dictM POST:YES ModelArray:nil HttpHeader:YES Results:^(ZLSessionManagerErrorState sessionErrorState, id responseObject) {
+        if (!sessionErrorState) {
+            [UserDataNew signOutNoAlert];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [ZLWarnView showErrorMessageOnView:UIApplication.sharedApplication.delegate.window Message:@"账号已注销成功"];
+            });
+            return;
+        }
+        [ZLWarnView showErrorMessageOnView:UIApplication.sharedApplication.delegate.window Message:@"请求失败"];
+    }];
+}
 
 @end
